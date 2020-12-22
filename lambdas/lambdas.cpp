a4a74d86bc69681184b8dbdae78803e0618b1060
ganyongchao <1056616678@qq.com>
2020-12-22 23:40:45 +0800

    frist push

---
+++ lambdas/lambdas.cpp
@@ -0,0 +1,31 @@
+﻿// lambdas.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void forFach(const std::vector<double>& vect,void(*func)(double))
+{
+	for (double it : vect)
+		func(it);
+}
+
+int main()
+{
+    //lambdas使用的小例子
+	std::vector<double> vect = { 3,1,2,4,5 };
+	//[] 是捕获 （）是参数列表
+	forFach(vect, [](double value)
+	{
+			std::cout << "value: " << value << std::endl;
+	});
+
+	//在vector中依次选择元素与3进行比较，当第一次出现比3大的数时，返回该数
+	//在  [](double v) {return v > 3; }  中，v是形式参数，实参是vect的迭代器。
+	auto it=std::find_if(vect.begin(), vect.end(), [](double v) {return v > 3; });
+	std::cout << *it << std::endl;
+	std::cin.get();
+}
+
+
---
+++ templateTest/templateTest.cpp
@@ -0,0 +1,54 @@
+﻿// templateTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+//宏定义的使用
+#define WAIT std::cin.get();
+//用法1、函数模板
+template<typename T>
+void print(T info)
+{
+	std::cout << info << std::endl;
+}
+
+//用法2、类模板 在类中使用
+//2.1、
+template<int N>
+class Array
+{
+private:
+	int my_array[N];
+public:
+	int get_size() const
+	{
+		return N;
+	}
+};
+//2.2
+template<typename T,int N>
+class Array2
+{
+private:
+	T my_array[N];
+public:
+	int get_size() const
+	{
+		return N;
+	}
+};
+int main()
+{
+	print(5);
+	print("hello");
+	print<float>(3.1f);
+
+	Array<8> a;
+	std::cout << a.get_size() << std::endl;
+	Array2<double, 6> a2;
+	std::cout << a2.get_size() << std::endl;
+	WAIT
+	return 0;
+	
+}
+
+
